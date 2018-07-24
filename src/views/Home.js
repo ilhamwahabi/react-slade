af78f894808ec25e52e72942e296be24eb3db1a7
Ilham Wahabi <wahabiputra@gmail.com>
2018-07-24 23:39:15 +0700

    Basic view, add direction & backdrop & dialog

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import Slade from '../../lib/Slade'
 
 import Container from '../components/Container'
 import Button from '../components/Button';
 
 class Home extends Component {
+  state = {
+    open: false,
+    index: 1
+  }
+
   render() {
     return (
       <Container>
         <Button>Show Dialog</Button>
+        <Slade open={this.state.open} index={this.state.index} />
       </Container>
     )
   }
