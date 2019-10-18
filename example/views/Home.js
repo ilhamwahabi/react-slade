3bfa6699a541ed161c68cb18887d8e1d428160eb
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-19 01:22:32 +0700

    remove customization example section

--- example/views/Home.js
+++ example/views/Home.js
@@ -1,30 +1,18 @@
 import React, { Component, Fragment } from 'react';
+
 import Slade from '../../lib/Slade';
 
 import Basic from '../sections/Basic';
-import Customized from '../sections/Customized';
 import Image from '../components/Image';
 
 import drastle1 from '../assets/drastle1.png';
 import drastle2 from '../assets/drastle2.png';
 import drastle3 from '../assets/drastle3.png';
-import drago1 from '../assets/drago1.png';
-import drago2 from '../assets/drago2.png';
-import drago3 from '../assets/drago3.png';
-
-const bg = 'linear-gradient(to right, #00c6ff, #0072ff)';
-const customArrow = 'https://puu.sh/B4aGY/132a4ca991.png';
 
 const basicItems = [
   <Image src={drastle1} />,
   <Image src={drastle2} />,
   <Image src={drastle3} />,
-];
-
-const customItems = [
-  <Image src={drago1} />,
-  <Image src={drago2} />,
-  <Image src={drago3} />,
 ];
 
 class Home extends Component {
@@ -36,21 +24,15 @@ class Home extends Component {
     arrow: undefined,
   }
 
-  setItems = (type, i) => {
-    switch (type) {
-      case 'basic': return this.setState({ items: basicItems });
-      case 'custom': return this.setState({
-        index: i, items: customItems, backdrop: bg, arrow: customArrow,
-      });
-      default: return [];
-    }
+  setItems = () => {
+    this.setState({ items: basicItems });
   }
 
-  openSlade = (type, i) => { this.setItems(type, i); this.setState({ open: true }); }
+  openSlade = () => { this.setItems(); this.setState({ open: true }); }
 
   closeSlade = () => {
     this.setState({
-      open: false, items: [], index: 0, backdrop: undefined, arrow: undefined,
+      open: false, index: 0, backdrop: undefined, arrow: undefined,
     });
   }
 
@@ -66,7 +48,6 @@ class Home extends Component {
     return (
       <Fragment>
         <Basic openSlade={this.openSlade} />
-        <Customized openSlade={this.openSlade} />
         <Slade
           open={open}
           items={items}
